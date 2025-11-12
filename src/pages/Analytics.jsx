595624cff7f9f0756c07a34c26d907009a6e1091
rohityadav112990 <157958354+rohityadav112990@users.noreply.github.com>
2025-11-12 20:11:14 +0530

    Create Analytics.jsx

---
+++ src/pages/Analytics.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AnalyticsPanel from '../components/AnalyticsPanel';
+
+const Analytics = ({ accentColor }) => {
+  return <AnalyticsPanel accentColor={accentColor} />;
+};
+
+export default Analytics;
